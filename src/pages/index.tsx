dcf2a27574e7bc7cb7f285d415070c910704cb55
Felipe S Santana <felipedb91@gmail.com>
2021-07-15 08:17:51 -0300

    feat: configure Chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
+import { Button } from '@chakra-ui/react';
 
 export default function Home() {
   return (
@@ -22,10 +23,10 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          <Button as="a" href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          </Button>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
@@ -65,5 +66,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }

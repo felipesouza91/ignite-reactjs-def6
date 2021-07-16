f71c1434c4d9feacfbb94afc502415152188313c
Felipe S Santana <felipedb91@gmail.com>
2021-07-16 14:02:19 -0300

    feat: create a Header and Banner structure

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Flex, Image } from '@chakra-ui/react';
+
+import React from 'react';
+import styles from './Header.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <Flex w="1440" h="50" justifyContent="center" alignItems="center">
+      <Image src="/Logo.png" />
+    </Flex>
+  );
+};
+
+export default Header;
